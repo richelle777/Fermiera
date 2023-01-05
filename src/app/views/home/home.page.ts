895ae9fbcb16c3068f26e88fdfffb2e142c91d5d
richelle777 <richellemayap@gmail.com>
2023-01-05 15:19:24 +0100

    version avec dynamisation de la route partant de l'accueil pour la liste des articles

--- src/app/views/home/home.page.ts
+++ src/app/views/home/home.page.ts
@@ -1,5 +1,6 @@
 import { HttpService } from './../../services/public1/http.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,11 +11,10 @@ export class HomePage implements OnInit {
   articles : any;
   categories : any;
   resultsOfFilter  = [];
-  constructor(private httpSevice:HttpService) { 
+  constructor(private httpSevice:HttpService , private router:Router) { 
     this.httpSevice.listArticles().then((data) => {
       this.articles = data
-      console.log(this.articles);
-      
+     // console.log(this.articles);
     })
   }
 
@@ -25,7 +25,7 @@ export class HomePage implements OnInit {
     this.httpSevice.listCategories().then((data) => {
       this.categories = data
       this.resultsOfFilter = this.categories;
-      console.log(this.categories);
+      //console.log(this.categories);
     })
   }
 
@@ -33,5 +33,12 @@ export class HomePage implements OnInit {
     const query = event.target.value.toLowerCase();
     this.resultsOfFilter = this.categories.filter(d => d.nom.toLowerCase().indexOf(query) > -1);
     console.log(this.resultsOfFilter);
+  }
+
+  toPage(obj) {
+    localStorage.setItem("categorie", JSON.stringify(obj));
+    console.log(obj);
+    this.router.navigate(['liste-articles'])
+    // this.router.navigate(['tab/liste-articles/',obj.nom])
   }
 }
--- src/app/views/liste-articles/liste-articles.page.ts
+++ src/app/views/liste-articles/liste-articles.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-liste-articles',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./liste-articles.page.scss'],
 })
 export class ListeArticlesPage implements OnInit {
+  categorieSelected : any;
 
-  constructor() { }
-
+  constructor(private route: ActivatedRoute) { 
+   
+  }
+  ionViewDidEnter(){
+    const retrieve = localStorage.getItem("categorie");
+    this.categorieSelected = JSON.parse(retrieve);
+    // this.categorieSelected = this.route.snapshot.paramMap.get('categorie');
+    // console.log(this.categorieSelected);
+    
+  }
   ngOnInit() {
   }
+
 
 }
