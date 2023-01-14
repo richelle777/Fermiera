bdc41591e3d1d6e8b24e6d7c84c9de68055a52c8
siewe linda <stephanie.siewe@institutsaintjean.org>
2023-01-14 22:15:02 +0100

    addresses page already for user cu0117

---
+++ src/app/views/add-address/add-address.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+@Component({
+  selector: 'app-add-address',
+  templateUrl: './add-address.page.html',
+  styleUrls: ['./add-address.page.scss'],
+})
+export class AddAddressPage implements OnInit {
+
+  region: string;
+
+  constructor(private modalCtrl: ModalController) {}
+
+  cancel() {
+    return this.modalCtrl.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    return this.modalCtrl.dismiss(this.region, 'confirm');
+  }
+
+  ngOnInit() {
+  }
+
+}
