ee17fc0fee3cae748b6587c08d755cf899f8796d
Tagne237 <elohim.tagne@institutsaintjean.org>
2023-01-18 18:19:04 -0100

    setting

--- src/app/views/setting/setting.page.ts
+++ src/app/views/setting/setting.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-setting',
   templateUrl: './setting.page.html',
@@ -13,17 +14,22 @@ export class SettingPage implements OnInit {
     motDePasse: new FormControl(),
   });
 
-  constructor() { }
+  constructor(private authService:AuthService) { }
   user:any;
   userbody:any;
+  customer:any;
   ngOnInit() {
     const retrieve=localStorage.getItem('customer');
     // @ts-ignore
     this.user=JSON.parse(retrieve);
     this.userbody=this.user.body;
   }
-  async save(){
+   save(){
     let userInfo={"nom":this.Info.value.nom,"email":this.Info.value.email,"motDePasse":this.Info.value.motDePasse}
-   
+    ;
+     this.authService.updateCustommer(userInfo).subscribe((data)=>{
+      this.customer=data;
+      localStorage.setItem("customer", JSON.stringify(this.customer));
+     })
   }
 }
